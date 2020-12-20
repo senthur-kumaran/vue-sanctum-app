2774e387e59e9ca32b87de9f1c6ca9c77b3e95bd
senthurkumaran <senthurkumaran@adventus.io>
2020-12-20 12:20:54 +0530

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <form action="#" method="post" @submit.prevent="login">
+      <div>
+        <input type="email" name="email" placeholder="email">
+      </div>
+      <div>
+        <input type="password" name="password" placeholder="password">
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000';
+
+export default {
+  name: 'Home',
+  methods: {
+    login() {
+      axios.get('/sanctum/csrf-cookie').then(response => {
+          axios.post('/login', {
+            email: 'senthur@gmail.com',
+            password: 'test1234',
+          })
+          .then(resposne => {
+            this.$router.push({ name: 'Dashboard' })
+          })
+      });
+    }
+  }
+}
+</script>
